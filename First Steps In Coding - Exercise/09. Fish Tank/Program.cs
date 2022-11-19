d99bb34a0fb7cace2a144e1ba74d36f1ff84ecb5
venci0003 <venci0003@gmail.com>
2022-11-19 20:59:22 +0200

    Refactored solution - 09. Fish Tank

--- First Steps In Coding - Exercise/09. Fish Tank/Program.cs
+++ First Steps In Coding - Exercise/09. Fish Tank/Program.cs
@@ -6,7 +6,19 @@ namespace _09._Fish_Tank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double volume = lenght * width * height;
+
+            double volumeInLiters = volume / 1000;
+
+            double busy = percentage / 100;
+
+            double liters = volumeInLiters * (1 - busy);
+            Console.WriteLine(liters);
         }
     }
 }
