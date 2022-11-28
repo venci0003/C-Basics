b2a801aa132192a6ee9edc2cbb26e74c2f4bb35d
venci0003 <venci0003@gmail.com>
2022-11-28 21:02:28 +0200

    Uploaded solutions to - Nested Loops - Lab

---
+++ Nested Loops - Lab/02. Multiplication Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int firstNumber = 1; firstNumber <= 10; firstNumber++)
+            {
+                for (int secondNumber = 1; secondNumber <= 10; secondNumber++)
+                {
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                }
+            }
+        }
+    }
+}
---
+++ Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loops = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int firstNumber = 0; firstNumber <= loops; firstNumber++)
+            {
+                for (int secondNumber = 0; secondNumber <= loops; secondNumber++)
+                {
+                    for (int thirdNumber = 0; thirdNumber <= loops; thirdNumber++)
+                    {
+                        if (firstNumber + secondNumber + thirdNumber == loops)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
